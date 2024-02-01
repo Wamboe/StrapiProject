04f2e9d012834c6d26cc60d721b97634d93bbed7
Sheila Karienye <sheila.karienye@gmail.com>
2024-02-01 09:20:20 +0300

    Initial commit

---
+++ src/api/muziki/routes/muziki.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * muziki router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::muziki.muziki');
---
+++ src/api/muziki/services/muziki.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * muziki service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::muziki.muziki');
